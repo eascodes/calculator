7c2a85f41f2c94d073f7d4410e4da54be839dacc
Emily <easchoettler@gmail.com>
2022-10-11 13:14:34 -0400

    Add basic calculation functions

---
+++ myScript.js
@@ -0,0 +1,17 @@
+function add(a, b) {
+    return a + b;
+  };
+  
+function subtract(a, b) {
+    return a - b;
+  };
+
+function multiply(a, b) {
+    return a * b;
+  };
+
+function divide(a, b) {
+    if (b === 0) alert("You can't do that!");
+    return a / b;
+  };
+
