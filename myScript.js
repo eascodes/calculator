73ad4264acc17b538fb42a835b498ffc86cb9270
Emily <easchoettler@gmail.com>
2022-10-12 11:35:44 -0400

    Updated function to store first result for addition & use to continue calculating

--- myScript.js
+++ myScript.js
@@ -29,29 +29,53 @@ let displayValue = "";
 let firstVal = "";
 let operator = "";
 let secondVal = "";
-let newDisplay = "";
-buttons.forEach(button => button.addEventListener("click", function(e) {
+//let newDisplay = "";
+
+getFirstVal();
+
+function getFirstVal() {buttons.forEach(button => button.addEventListener("click", startCalc));}
+
+function startCalc(e) {
     if(e.target.className == "buttons num") {
         displayValue += e.target.textContent;
         display.textContent = displayValue;
     } else if(e.target.className == "buttons oper") {
-        firstVal = parseInt(displayValue);
+        firstVal = parseInt(display.textContent);
         operator = e.target.textContent;
+        console.log(firstVal);
         display.textContent = e.target.textContent;
-
-        buttons.forEach(button => button.addEventListener("click", function(e) {
-            if(e.target.className == "buttons num") {
-                newDisplay += e.target.textContent;
-                display.textContent = newDisplay;
-            } else if (e.target.id == "equal") {
-                secondVal = parseInt(newDisplay);
-                console.log(operator);
-                if(operator == "+") {
-                    display.textContent = add(firstVal, secondVal);
-                }
-            }
-        }))
+        finishCalc();
     }
-}));
+}
 
+function finishCalc() {
+    displayValue = "";
+    buttons.forEach(button => button.removeEventListener("click", startCalc))
+    buttons.forEach(button => button.addEventListener("click", completeCalc))
+    }
+
+function completeCalc(e) {
+    if(e.target.className == "buttons num") {
+        displayValue += e.target.textContent;
+        display.textContent = displayValue;
+    } else if (e.target.id == "equal") {
+        secondVal = parseInt(displayValue);
+        if(operator == "+") {
+            firstResult = add(firstVal, secondVal);
+            display.textContent = firstResult;
+            displayValue = "";
+            getFirstVal();
+        } else if(operator == "-") {
+            display.textContent = subtract(firstVal, secondVal);
+        } else if(operator == "*") {
+            display.textContent = multiply(firstVal, secondVal);
+        } else if(operator == "/") {
+            display.textContent = divide(firstVal, secondVal);
+        }
+    }
+}
+
+function calculate(first) {
+
+}
 
