7479caade0ef07aff6fcae73993b1eebd898c8a8
Emily <easchoettler@gmail.com>
2022-10-13 13:51:19 -0400

    Re-organize and simplify code/functions to make more intuitive

--- myScript.js
+++ myScript.js
@@ -1,3 +1,16 @@
+//Setting variables
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll(".buttons");
+const clear = document.querySelector("#AC");
+const back = document.querySelector("#C");
+const dot = document.querySelector("#dot");
+const percent = document.querySelector("#percent");
+let displayValue = "";
+let firstVal = "";
+let operator = "";
+let secondVal = "";
+
+//Basic calculation functions
 function add(a, b) {
     return Number(Math.round((a + b) + 'e8') + 'e-8');
   };
@@ -22,19 +35,7 @@ function divide(a, b) {
     else if (operator == "/") {return divide(num1, num2);}
   }
 
-const display = document.querySelector(".display");
-const buttons = document.querySelectorAll(".buttons");
-const clear = document.querySelector("#AC");
-const back = document.querySelector("#C");
-const dot = document.querySelector("#dot");
-const percent = document.querySelector("#percent");
-let displayValue = "";
-let firstVal = "";
-let operator = "";
-let secondVal = "";
-
-setInitialEvent();
-
+//Special button functionality
 clear.addEventListener("click", clearDisplay);
 function clearDisplay() {
     display.textContent = "";
@@ -66,63 +67,83 @@ function clearDisplay() {
     display.textContent = Number(Math.round((displayValue / 100) + 'e8') + 'e-8');
  }
 
+//Calculation functionality w/ user interface
+setInitialEvent();
+
+function setInitialEvent() {buttons.forEach(button => button.addEventListener("click", startCalc));}
+
+function startCalc(e) {
+    if(e.target.className == "buttons num") {
+        displayFirstNums(e);
+    } else if(e.target.className == "buttons oper") {
+        storeOperator(e);
+    }
+}
+
+function completeCalc(e) {
+    if(e.target.className == "buttons num") {
+        displaySecondNums(e);
+    } else if (e.target.className == "buttons oper") {
+        storeNextOperator(e);
+    } else if (e.target.id == "equal") {
+        displayFinalResult(e);
+    }
+}
+
 function removeEvents() {
     buttons.forEach(button => button.removeEventListener("click", completeCalc))
     buttons.forEach(button => button.removeEventListener("click", startCalc))
 }
 
-function setInitialEvent() {buttons.forEach(button => button.addEventListener("click", startCalc));}
-
-function removeClass() {
+function removeSelectedClass() {
     buttons.forEach(button => { 
         if(button.classList.contains("selected")) {
             button.classList.remove("selected");
     }});
 }
 
-function startCalc(e) {
-    if(e.target.className == "buttons num") {
-        displayValue += e.target.textContent;
-        display.textContent = displayValue;
-    } else if(e.target.className == "buttons oper") {
-        firstVal = parseFloat(display.textContent);
+function displayFirstNums(e) {
+    displayValue += e.target.textContent;
+    display.textContent = displayValue;
+}
+
+function storeOperator(e) {
+    firstVal = parseFloat(display.textContent);
         operator = e.target.textContent;
         e.target.classList.add("selected");
         resetEvents();
-    }
 }
 
 function resetEvents() {
     displayValue = "";
-    buttons.forEach(button => button.removeEventListener("click", startCalc))
-    buttons.forEach(button => button.addEventListener("click", completeCalc))
+    buttons.forEach(button => button.removeEventListener("click", startCalc));
+    buttons.forEach(button => button.addEventListener("click", completeCalc));
     }
 
-function completeCalc(e) {
-    if(e.target.className == "buttons num") {
-        removeClass();
-        displayValue += e.target.textContent;
-        display.textContent = displayValue;
-    } else if (e.target.className == "buttons oper") {
-        e.target.classList.add("selected");
-        secondVal = parseFloat(displayValue);
-        firstResult = operate(firstVal, operator, secondVal);
-        operator = e.target.textContent;
-        firstVal = firstResult;
-        display.textContent = firstResult;
-        displayValue = "";
-        buttons.forEach(button => button.removeEventListener("click", completeCalc))
-        resetEvents();
-    } else if (e.target.id == "equal") {
-        buttons.forEach(button => button.removeEventListener("click", completeCalc))
-        secondVal = parseFloat(displayValue);
-        if (operator == "+" || operator == "-" || operator == "*" || operator == "/") {
-            firstResult = operate(firstVal, operator, secondVal);
-            display.textContent = firstResult;
-            displayValue = "";
-            setInitialEvent();
-        } 
-    }
+function displaySecondNums(e) {
+    removeSelectedClass();
+    displayValue += e.target.textContent;
+    display.textContent = displayValue;
 }
 
+function storeNextOperator(e) {
+    e.target.classList.add("selected"); //highlight the selected operator button
+    secondVal = parseFloat(displayValue);
+    firstResult = operate(firstVal, operator, secondVal); //calculate result of first expression
+    display.textContent = firstResult; //display result of first expression
+    firstVal = firstResult; //store first result as the first value in the next expression
+    operator = e.target.textContent; //store operator for next expression
+    buttons.forEach(button => button.removeEventListener("click", completeCalc));
+    resetEvents();
+}
 
+function displayFinalResult() {
+    buttons.forEach(button => button.removeEventListener("click", completeCalc))
+    secondVal = parseFloat(displayValue);
+    if (operator == "+" || operator == "-" || operator == "*" || operator == "/") {
+        firstResult = operate(firstVal, operator, secondVal);
+        display.textContent = firstResult;
+        displayValue = "";
+        setInitialEvent();
+    } 
+}
